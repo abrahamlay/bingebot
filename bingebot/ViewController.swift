a1681f76a8e95b790ba91629cc6a458e0b624660
abrahamlay <abraham_lay@yahoo.co.id>
2020-04-05 22:28:06 +0700

    Initial Commit

---
+++ bingebot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bingebot
+//
+//  Created by Abraham Lay (ID) on 05/04/20.
+//  Copyright © 2020 Abraham Lay (ID). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
