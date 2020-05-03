daec1c5bd6b08b8bd4dac259e5706bae2e5589bb
abrahamlay <abraham_lay@yahoo.co.id>
2020-05-03 16:39:18 +0700

    finish bingebot app

--- bingebot/ViewController.swift
+++ bingebot/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showsLabel: UILabel!
+    
+    @IBOutlet weak var randomedShowLabel: UILabel!
+    
+    @IBOutlet weak var bingebotSpokenLabel: UILabel!
+    
+    @IBOutlet weak var enterShowTextField: UITextField!
+    
+    @IBOutlet weak var addShowButton: UIButton!
+    
+    @IBOutlet weak var randomShowStackView: UIStackView!
+    
+    @IBOutlet weak var addShowStackView: UIStackView!
+    
+    @IBOutlet weak var showsStackView: UIStackView!
+    
+    @IBOutlet weak var randomShowButton: UIButton!
+    
+    var shows :[String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showsStackView.isHidden = true
+        randomShowStackView.isHidden = true
+        bingebotSpokenLabel.isHidden = true
+        randomedShowLabel.isHidden = true
     }
 
-
+    @IBAction func randomBingeShowButtonClicked(_ sender: Any) {
+        randomedShowLabel.text = shows.randomElement()
+        randomedShowLabel.isHidden = false
+        bingebotSpokenLabel.isHidden = false
+    }
+    @IBAction func onAddShowButtonClicked(_ sender: Any) {
+        guard let showName = enterShowTextField.text else {return}
+        if enterShowTextField.text != ""{
+            shows.append(showName)
+            updateShowsLabel()
+            enterShowTextField.text = ""
+            showsStackView.isHidden = false
+            
+            if shows.count > 1{
+                randomShowStackView.isHidden = false
+            }
+        }
+    }
+    
+    func updateShowsLabel(){
+        showsLabel.text = shows.joined(separator: ", ")
+    }
 }
 
